53baeac052073d3762a0d6d4059585da00720269
mouni <mounika.g1162@gmail.com>
2023-02-17 01:32:16 +0530

    link

--- task5/src/global.js
+++ task5/src/global.js
@@ -1,2 +1,3 @@
-export const API="http://localhost:4000"
+// export const API="http://localhost:4000"
+export const API="https://node-task5-92r1.vercel.app"
 export const API2="http://localhost:3000"
