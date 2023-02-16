62b17b359b1210ba15012a0b7b58c0e8ea2056e7
mouni <mounika.g1162@gmail.com>
2023-02-17 01:34:18 +0530

    ch

--- task5/src/email.js
+++ task5/src/email.js
@@ -18,9 +18,9 @@ export default function Email()
             // console.log(data);
          
             
-            toast("Link Sent to mail")
         })
         console.log(values);
+        toast("Link Sent to mail")
   
     }
 
