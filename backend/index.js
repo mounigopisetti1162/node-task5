07bccdb8651f2d81a9e9d2c8db56c7387a69eb02
mouni <mounika.g1162@gmail.com>
2023-02-17 16:28:53 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -66,12 +66,7 @@ app.get('/user',async function(request,responce)
 
 app.post('/user',async function(req,res)
 {
-    console.log("hello")
     const {firstname,email,lastname,password,confrimpassword}=req.body;
-    // const found=getuser(email)
-    // console.log(found)
-    // if(true)
-    // {
     const hashpassword=await generatehashedpassword(password)
     const hashpassword2=await generatehashedpassword(confrimpassword)
       //  db.movies.insertMany(data)
@@ -79,10 +74,6 @@ app.post('/user',async function(req,res)
     const newuser = await addnewuser(firstname,lastname,email,hashpassword,hashpassword2)
     console.log(newuser)
       res.send(newuser)
-    // }
-    // else{
-    //     res.status(401).send({message:"this user alredy exist"})
-    // }
 })
 app.post('/login',async function(request,responce)
 {
